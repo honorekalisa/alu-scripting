051c429327656502a7b5510083624ffed6bcc8e5
honorekalisa <h.kalisa@alustudent.com>
2023-07-16 20:30:30 +0200

    3-count

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ 3-count.py """
-import json
 import requests
 
 
 def count_words(subreddit, word_list, after="", count=[]):
-    """ keywords """
+    """ prints a sorted count of given keywords """
 
     if after == "":
         count = [0] * len(word_list)
